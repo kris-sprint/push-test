c721c31680b8ae1fd0d23d7305e7a831e3ba509c
kris-sprint <k.haronek@reply.com>
2024-10-23 14:19:38 +0100

    fix: manifest

--- push.js
+++ push.js
@@ -13,11 +13,11 @@ push.setVapidDetails("mailto:test@code.co.uk", vapidKeys.publickKey, vapidKeys.p
 // the subscriptions (vapidDetails in the types) of users that we want to recieve the notification, saved in a db
 let windowsSub = {
   endpoint:
-    "https://fcm.googleapis.com/fcm/send/fzR9qvwJ4HY:APA91bGEHVlXmcejF0pZqH5VDg6mi_lot4U8bf64Ho4oY-9fWqW3cJfgvdwW82rvREMECtu4dILXKae6aIedPtT7smIrgmY5r3cPC-5k9vUaFakn61sbIIjUMtz_PGGKWZZS-wZ-LXoR",
+    "https://fcm.googleapis.com/fcm/send/ckLo8LaI9MY:APA91bH_TITVreaVgDjtWLw3B8FMyCnqzGugNiwKHWkk5hzRnzWKEnhQfD0wUWKruWMSee-HxrZBNwqorQvpKfISG6_xr_n9BdaCZ_tXT0p1aU6OPU5n9yqYfRSxRtbyVvvB1-NavH6t",
   expirationTime: null,
   keys: {
-    p256dh: "BHcqMc2jExhor3Ru4TpKr901Z7vei_t2yrTQO9tI9qPUMHZsEk0kdleEner3_m7XM-fp6ALgLqM1fzRnEkzmm9c",
-    auth: "q4ILycan0w6ZHAaLt2IHzA",
+    p256dh: "BGU-EzkfVikMTybVgjlzIYqj457IzB8Up4vOTGo_UcI1aHZgU5QvycjR2OonUE7hYcnbt_LRw7pX85-o6Oi4C4s",
+    auth: "ynHaSHuifNPhsvH6IcqG2g",
   },
 };
 let iosSub = {
@@ -26,6 +26,16 @@ let iosSub = {
   keys: {
     p256dh: "BGyNJi3TUCh1TFfrUQRJ2NlfzAXohWY8wQld5LeEFk43k9NYt4IikRQ47xgd-WdIKPFzUNfhdVuWWCv0vvDxpGg",
     auth: "xxjxhx9R_3gJyP7BZg_hQA",
+  },
+};
+
+let androidSub = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/dNHOGui8TmI:APA91bFcvSUD54GmVsFzwhLxmgf2SHxT9pj-IMZC3CIyhosLGkdI2mIJ1xNcLDyYQSk_K9kAkzQiVNwWoqdKnWFbv_DZU9nU52uawtQK9aBcAv6OttJnUXfGqqFGBneJtc1_xsWvGJDP",
+  expirationTime: null,
+  keys: {
+    p256dh: "BHj4g7d7iSsIaE1sIfqWHQQOMs0Qata6YyMXpFjc6FwiiG5k2MhmoXl-KC1JiJ2ac0cmhaBsRtCKI3xB7mjZztU",
+    auth: "Gg1phCI_WlFc4H-yXQDFtg",
   },
 };
 
@@ -40,12 +50,12 @@ let payload = JSON.stringify({
     primaryKey: "2",
   },
   actions: [
-    { action: "explore", title: "Explore this new world", icon: "images/checkmark.png" },
+    { action: "explore", title: "Open", icon: "images/checkmark.png" },
     { action: "close", title: "Close", icon: "images/xmark.png" },
   ],
 });
 
 push
-  .sendNotification(iosSub, payload)
+  .sendNotification(windowsSub, payload)
   .then(() => console.log("Notification sent successfully"))
   .catch((err) => console.error("Error sending notification", err));
