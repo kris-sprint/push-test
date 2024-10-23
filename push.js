bf4c9780a2818c01dc95aa746c0fffd5ad77f8f8
kris-sprint <k.haronek@reply.com>
2024-10-23 10:43:20 +0100

    feat: pwa manifest

--- push.js
+++ push.js
@@ -13,11 +13,11 @@ push.setVapidDetails("mailto:test@code.co.uk", vapidKeys.publickKey, vapidKeys.p
 // the subscriptions of users that we want to recieve the notification, saved in a db
 let sub = {
   endpoint:
-    "https://fcm.googleapis.com/fcm/send/fzR9qvwJ4HY:APA91bGEHVlXmcejF0pZqH5VDg6mi_lot4U8bf64Ho4oY-9fWqW3cJfgvdwW82rvREMECtu4dILXKae6aIedPtT7smIrgmY5r3cPC-5k9vUaFakn61sbIIjUMtz_PGGKWZZS-wZ-LXoR",
+    "https://fcm.googleapis.com/fcm/send/f_LpfP26yb8:APA91bHzzJuQdm-Ytx0SjzxEqtT3mhVIamvaKl4TSjsP54UWKuqKi9Wl0aPh0KnVfzR0zFG8fGVcx1QMJYqKUDbi-9LwVMdMchV7dgo2v3HmR0X7q5SH2-QrQ6Yg5hjE5BO7IkLhwnvS",
   expirationTime: null,
   keys: {
-    p256dh: "BHcqMc2jExhor3Ru4TpKr901Z7vei_t2yrTQO9tI9qPUMHZsEk0kdleEner3_m7XM-fp6ALgLqM1fzRnEkzmm9c",
-    auth: "q4ILycan0w6ZHAaLt2IHzA",
+    p256dh: "BPjh1lUsYHh7y7l5fmcFD8nOvDBN9K9o9x3gm7bmqGcDWuxZUd6fvZx-8S9FnCBYV1eo8qqkQHOELlbMF5jLndM",
+    auth: "cKjKI9vQ5HY51W5QTSMRmw",
   },
 };
 
